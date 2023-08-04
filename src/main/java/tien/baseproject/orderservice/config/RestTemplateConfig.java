be12b0e567671a2fb056724721cd89775a184d4b
Vuong Tien <>
2023-08-04 16:07:19 +0700

    ioioi

--- src/main/java/tien/baseproject/orderservice/config/RestTemplateConfig.java
+++ src/main/java/tien/baseproject/orderservice/config/RestTemplateConfig.java
@@ -1,9 +1,11 @@
 package tien.baseproject.orderservice.config;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -14,4 +16,8 @@ public class RestTemplateConfig {
     public RestTemplate webClientBuilder() {
         return new RestTemplate();
     }
+
+    //@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    //Một bean với prototype scope sẽ trả về các instance khác nhau mỗi khi có một
+    //yêu mới sử dụng chúng đến IoC container.
 }
